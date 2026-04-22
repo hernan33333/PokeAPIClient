04e08e6197307e111bfd254c97184c5b329bdfc4
hernan33333 <brunohernan03@hotmail.com>
2026-04-22 17:05:15 -0600

    Componente de carga inicial

---
+++ src/app/Components/carga-component/carga-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carga-component',
+  imports: [],
+  templateUrl: './carga-component.html',
+  styleUrl: './carga-component.css',
+})
+export class CargaComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CargaComponent } from './Components/carga-component/carga-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path : "", component : CargaComponent}
+
+];
