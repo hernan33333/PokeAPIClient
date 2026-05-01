a4dfa2544deb0258b935a9d62aa6044c0fecf348
Miguel01-2003 <miguelpastrana16@outlook.com>
2026-04-30 18:40:30 -0600

    Correccciones de unificacion

--- src/app/Componentes/pokemon/pokemon.ts
+++ src/app/Componentes/pokemon/pokemon.ts
@@ -6,13 +6,9 @@ import { TipoModel } from '../../Modelos/tipo-model';
 import { PokemonCartaComponent } from '../pokemon-carta/pokemon-carta';
 import { Router } from '@angular/router';
 import { RegionModel } from '../../Modelos/region-model';
-import { RegionesService } from '../../Servicios/region-service';
 import { GeneracionModel } from '../../Modelos/generacion-model';
 import { GeneracionService } from '../../Servicios/generacion-service';
 import { RouterLink } from '@angular/router';
-import { GeneracionService } from '../../Servicios/generacion-service';
-import { GeneracionModel } from '../../Modelos/generacion-model';
-import { RegionModel } from '../../Modelos/region-model';
 import { RegionService } from '../../Servicios/region-service';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -33,24 +29,24 @@ export class Pokemon implements OnInit {
   public regiones: RegionModel[] = [];
   private formularioReactivo = inject(FormBuilder);
 
-  public RegionDefecto: RegionModel ={
-    id:0,
-    Generacion:0,
+  public RegionDefecto: RegionModel = {
+    id: 0,
+    Generacion: 0,
     Nombre: "Ninguna opcion seleccionada"
   }
 
-  public GeneracionDefecto: GeneracionModel ={
-    Id:0,
-    Nombre:"Ninguna opcion seleccionda",
+  public GeneracionDefecto: GeneracionModel = {
+    Id: 0,
+    Nombre: "Ninguna opcion seleccionda",
     Region: this.RegionDefecto
   }
 
-  public TipoDefecto: TipoModel ={
+  public TipoDefecto: TipoModel = {
     Id: 0,
     Nombre: "Ninguna opcion seleccionada",
     Generacion: 0
   }
-  
+
   public form: FormGroup = this.formularioReactivo.group({
     Nombre: [''],
     Region: [this.RegionDefecto],
@@ -60,21 +56,21 @@ export class Pokemon implements OnInit {
 
   offset: number = 19;
   paso: number = 19;
-  
+
   constructor(
-    private pokemonService: PokemonService, 
+    private pokemonService: PokemonService,
     private tiposService: TipoService,
-    private regionService: RegionesService,
+    private regionService: RegionService,
     private generacionService: GeneracionService,
     private router: Router,
     private cdr: ChangeDetectorRef
-  ){};
+  ) { };
 
 
 
   ngOnInit(): void {
 
-    if(this.pokemonService.pokemones.length <= 0){
+    if (this.pokemonService.pokemones.length <= 0) {
 
       this.router.navigate([""]);
 
@@ -122,16 +118,16 @@ export class Pokemon implements OnInit {
 
         this.pokemons = data.objects;
         this.cdr.detectChanges();
-        
+
       }
     )
   }
 
-  GetAll(){
+  GetAll() {
 
     this.pokemonService.getAll().subscribe(
 
-      pokemonesData =>{
+      pokemonesData => {
 
         this.pokemons = pokemonesData.objects;
         this.cdr.detectChanges();
@@ -146,7 +142,7 @@ export class Pokemon implements OnInit {
     )
   }
 
-  GetAllTipos(){
+  GetAllTipos() {
 
     this.tiposService.getAll().subscribe(
 
@@ -165,15 +161,15 @@ export class Pokemon implements OnInit {
     )
   }
 
-  GetAllRegiones(){
+  GetAllRegiones() {
 
     this.regionService.getAll().subscribe(
 
       regionesData => {
-        
+
         this.regiones = regionesData.objects;
         this.cdr.detectChanges();
-        
+
 
       },
       error => {
@@ -186,8 +182,8 @@ export class Pokemon implements OnInit {
 
   }
 
-  GetAllGeneraciones(){
-    
+  GetAllGeneraciones() {
+
     this.generacionService.getAll().subscribe(
 
       generacionesData => {
@@ -208,7 +204,7 @@ export class Pokemon implements OnInit {
 
   }
 
-  GetById(id: number){
+  GetById(id: number) {
 
     this.pokemonService.getById(id).subscribe(
 
@@ -219,21 +215,23 @@ export class Pokemon implements OnInit {
       }
     )
   }
-}
 
- busqueda(){
+  busqueda() {
     console.log(this.form.value)
     this.pokemon = this.form.value as PokemonModel;
     console.log(this.pokemon)
     this.pokemonService.busqueda(this.pokemon).subscribe({
-      next:(data) =>{
-        if(data.correct){
+      next: (data) => {
+        if (data.correct) {
           this.pokemons = data.objects;
           this.cdr.detectChanges();
-        }else{
-          console.log("error:"+data.errorMessage);
+        } else {
+          console.log("error:" + data.errorMessage);
         }
       }
     })
   }
 }
+
+
+
