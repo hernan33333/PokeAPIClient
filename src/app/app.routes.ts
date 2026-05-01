05109fe0a938752cb684fd247b67046539c54a34
Miguel01-2003 <miguelpastrana16@outlook.com>
2026-04-30 18:26:17 -0600

    LogIn

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { PokemonDetalles } from './Componentes/pokemon-detalles/pokemon-detalles
 import { Pokemon } from './Componentes/pokemon/pokemon';
 import { CargaComponent } from './Componentes/carga-component/carga-component';
 import { Login } from './Componentes/login/login';
+import { Component } from '@angular/core';
+import { RegistroUsuario } from './Componentes/registro-usuario/registro-usuario';
 
 export const routes: Routes = [
     
     {path : "", component : CargaComponent},
     {path: "pokemon",component:Pokemon},
     {path:"pokemon/:id", component: PokemonDetalles},
-    {path:"login", component: Login}
+    {path:"login", component: Login},
+    {path:"registro",component: RegistroUsuario}
 ];
